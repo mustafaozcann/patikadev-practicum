7f2d2e56b9403dc05e8056ab86dc3312c7bf95d5
mustafa özcan <mstffaozcnn@gmail.com>
2022-10-14 21:56:52 +0300

    patika-dev practicum

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -16,14 +16,6 @@ public class Employee {
 
     }
 
-    /*
-     * 
-     * Maaşa uygulanan vergiyi hesaplayacaktır
-     * Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
-     * Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi
-     * uygulanacaktır.
-     * 
-     */
     public float tax() {
         if (salary <= 1000) {
             return 0;
@@ -33,14 +25,6 @@ public class Employee {
 
     }
 
-    /*
-     * bonus() :
-     * Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her
-     * saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
-     * 
-     * 
-     * 
-     */
     public int bonus() {
         if (workHours > 40) {
             return (workHours - 40) * 30;
@@ -48,24 +32,6 @@ public class Employee {
         return 0;
     }
 
-    /*
-     * raiseSalary() :
-     * 
-     * 
-     * Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki
-     * yılı 2021 olarak alın.
-     * Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam
-     * yapılacaktır.
-     * Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam
-     * yapılacaktır.
-     * Eğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.
-     * 
-     * 
-     * NOT
-     * 
-     * VERGİ VE BONUSLAR DİKKATE ALINACAK
-     * 
-     */
 
     public float raiseSalary() {
 
@@ -86,15 +52,6 @@ public class Employee {
         return 0;
     }
 
-    // Çalışana ait bilgileri ekrana bastıracaktır.
-    /*
-     * 
-     * Adı, maaşı, çalışma saati, başlangıç yılı, vergi, bonus, maaş artışı, vergi
-     * ve bonuslarla birlikte maaş, toplam maaş değerlerini konsola yazdırmanızı
-     * bekliyoruz.
-     * 
-     * 
-     */
     @Override
     public String toString() {
         return "Adı : " + name + "\n" +
