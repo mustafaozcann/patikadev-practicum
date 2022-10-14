7e8d239fa442d3583a366ce5ad41abd4b76a8649
mustafa özcan <mstffaozcnn@gmail.com>
2022-10-14 21:55:37 +0300

    patika-dev practicum

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import models.Employee;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+
+        Employee employee1 = new Employee("Mustafa", 10000f, 46, 2021);
+        Employee employee2 = new Employee("Özcan", 900f, 46, 2021);
+
+
+        System.out.println(employee1);
+        System.out.println("***************************** PATİKA-DEV PRACTICUM *******************************");
+        System.out.println(employee2);
+
+
     }
 }
--- src/models/Employee.java
+++ src/models/Employee.java
@@ -1,82 +1,111 @@
 package models;
 
-public class Employee{
+public class Employee {
 
-    private String name; //çalışan ismi
-    private float salary; //çalışan maaşı
-    private byte workHours; //haftalık çalışma saati
-    private int hireYear; //işe başlangıç tarihi
+    private String name; 
+    private float salary; 
+    private int workHours; 
+    private int hireYear; 
 
-    //constructor
-    public Employee(String name,float salary, byte workHours,int  hireYear){
+    
+    public Employee(String name, float salary, int workHours, int hireYear) {
         this.name = name;
         this.salary = salary;
         this.workHours = workHours;
         this.hireYear = hireYear;
 
     }
-    
+
     /*
+     * 
+     * Maaşa uygulanan vergiyi hesaplayacaktır
+     * Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
+     * Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi
+     * uygulanacaktır.
+     * 
+     */
+    public float tax() {
+        if (salary <= 1000) {
+            return 0;
+        }
 
-    Maaşa uygulanan vergiyi hesaplayacaktır
-    Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
-    Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi uygulanacaktır.
+        return salary * 3 / 100;
 
-    */
-    public float tax(){
-
-
-        return 0;
     }
+
     /*
      * bonus() :
-     * Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
+     * Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her
+     * saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
      * 
      * 
      * 
      */
-    public int bonus(){
+    public int bonus() {
+        if (workHours > 40) {
+            return (workHours - 40) * 30;
+        }
         return 0;
     }
 
-
     /*
-     * raiseSalary() : 
-     * Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki yılı 2021 olarak alın.
-
-     * Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam yapılacaktır.
-        Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam yapılacaktır.
-        Eğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.
-
+     * raiseSalary() :
      * 
+     * 
+     * Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki
+     * yılı 2021 olarak alın.
+     * Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam
+     * yapılacaktır.
+     * Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam
+     * yapılacaktır.
+     * Eğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.
+     * 
+     * 
+     * NOT
+     * 
+     * VERGİ VE BONUSLAR DİKKATE ALINACAK
      * 
      */
 
-     public float raiseSalary(){
+    public float raiseSalary() {
+
+        int totalYear = 2021 - hireYear;
+        float taxAndBonus = bonus() - tax();
+        float salaryWithBonusesAndTaxes = salary + taxAndBonus;
+
+        if (totalYear < 10) {
+            return salaryWithBonusesAndTaxes * 5 / 100;
+        }
+        else if(totalYear > 9 && totalYear < 20){
+            return salaryWithBonusesAndTaxes * 10 / 100;
+        }
+        else if(totalYear > 19){
+            return salaryWithBonusesAndTaxes * 15 / 100;
+        }
 
         return 0;
-     }
+    }
 
-
-     //Çalışana ait bilgileri ekrana bastıracaktır.
-     /*
-      * 
- Adı, maaşı, çalışma saati, başlangıç yılı, vergi, bonus, maaş artışı, vergi ve bonuslarla birlikte maaş, toplam maaş değerlerini konsola yazdırmanızı bekliyoruz.
-
-
-      */
-     @Override
-     public String toString() {
-        return "Adı : " + name +
-        "Maaş : " + salary +
-        "Haftalık Çalışma saati : " + workHours +
-        "Başlangıç Yılı : " + hireYear +
-        "Vergi : " +
-        "Bonus : " +
-        "Maaş Artışı : " +
-        "Vergi ve bonuslarla birlikte maaş : " +
-        "Toplam Maaş : ";
-     }
-
+    // Çalışana ait bilgileri ekrana bastıracaktır.
+    /*
+     * 
+     * Adı, maaşı, çalışma saati, başlangıç yılı, vergi, bonus, maaş artışı, vergi
+     * ve bonuslarla birlikte maaş, toplam maaş değerlerini konsola yazdırmanızı
+     * bekliyoruz.
+     * 
+     * 
+     */
+    @Override
+    public String toString() {
+        return "Adı : " + name + "\n" +
+                "Maaş : " + salary + "₺\n" +
+                "Haftalık Çalışma saati : " + workHours +"\n" +
+                "Başlangıç Yılı : " + hireYear +"\n" +
+                "Vergi : " + tax() +"₺\n" +
+                "Bonus : " + bonus() +"₺\n" +
+                "Maaş Artışı : " + raiseSalary() +"₺\n" +
+                "Vergi ve bonuslarla birlikte maaş : " + (salary + bonus() - tax()) +"₺\n" +
+                "Toplam Maaş : " + (salary + bonus()) + "₺";
+    }
 
 }
